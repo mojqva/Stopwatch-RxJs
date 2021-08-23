4e163c2f387bc125431bd8d33d82de0cc9975646
mojqva <majanka@i.ua>
2021-08-23 08:34:19 +0300

    3/4 buttons are ready

--- src/App.js
+++ src/App.js
@@ -1,39 +1,72 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react';
+import {
+  interval,
+  Subject
+} from 'rxjs';
+import {
+  scan,
+  startWith,
+} from 'rxjs/operators';
+
+
 
 function App() {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState();
   const [timerOn, setTimerOn] = useState(false);
+  const [reset, setReset] = useState(false);
+
+  const observable$ = interval(10).pipe(
+    startWith(0),
+    scan(time => time + 10)
+  );
+
+  const action$ = new Subject();
+  action$.subscribe(setTime);
+
+  const actionOn$ = new Subject();
+  actionOn$.subscribe(setTimerOn);
+
+  const actionReset$ = new Subject();
+  actionReset$.subscribe(setReset);
 
   useEffect(() => {
-    let interval = null;
+    const sub = observable$.subscribe(setTime);
 
-    if(timerOn) {
-      interval = setInterval(() => {
-        setTime(prevTime => prevTime + 10)
-      }, 10)
-    } else {
-      clearInterval(interval)
+    if(!timerOn) {
+      sub.unsubscribe()
     }
+    
+    if(reset) {
+      action$.next(0);
+    }
+    actionReset$.next(false);
 
-    return () => clearInterval(interval)
-  }, [timerOn])
+    return () => sub.unsubscribe();
+    // let interval = null;
+
+    // if(timerOn) {
+    //   interval = setInterval(() => {
+    //     setTime(prevTime => prevTime + 10)
+    //   }, 10)
+    // } else {
+    //   clearInterval(interval)
+    // }
+
+    // return () => clearInterval(interval)
+  }, [timerOn, reset])
 
   return (
     <div>
       <div>
         <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{('0' + ((time / 10) % 100)).slice(-2)}</span>
+        <span>{('0' + ((time/10) % 100)).slice(-2)}</span>
       </div>
       <div>
-        <button onClick={() => setTimerOn(true)}>Start</button>
-        <button onClick={() => {
-          setTimerOn(false);
-          setTime(0);
-          }
-        }>Stop</button>
-        <button onDoubleClick={() => setTimerOn(false)}>Wait</button>
-        <button onClick={() => setTime(0)}>Reset</button>
+        <button onClick={() => {actionOn$.next(true)}}>Start</button>
+        <button onClick={() => {actionOn$.next(false)}}>Stop</button>
+        <button onDoubleClick={() => actionOn$.next(false)}>Wait</button>
+        <button onClick={() => actionReset$.next(true)}>Reset</button>
       </div>
     </div>
   );
